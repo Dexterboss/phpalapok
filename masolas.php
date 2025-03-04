58372f75b7a85e2e89e00d080505b88e934e8c1f
Desterboss <rolandmagyar78@gmail.com>
2025-03-04 09:11:07 +0100

    masolas.php futtatásra kész

---
+++ masolas.php
@@ -0,0 +1,16 @@
+<?php
+$tombBe=array("S", "Z", "I", "G", "M", "A");
+$tombKi=array("", "", "", "", "", "");
+
+$Betu="A";
+$indexMax=count($tombBe)-1; //Elemszám -1
+for ($i=0; $i <= $indexMax; $i++)
+{
+    $tombKi[$i]=$tombBe[$i].$Betu;
+    $tombKi[$i]=$tombKi[$i].$tombKi[$i];
+}
+    
+print($tombBe);
+echo "<br>";
+print_r($tombKi);
+echo "<br>";
